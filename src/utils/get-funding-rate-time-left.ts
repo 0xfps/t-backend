009746cd8c7b471cd473d5348c7154411a591504
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-02-21 20:34:45 +0100

    '✨✨✨✨'

--- src/utils/get-funding-rate-time-left.ts
+++ src/utils/get-funding-rate-time-left.ts
@@ -2,12 +2,12 @@ import fundingRatesModel from "../db/schema/funding-rates";
 import { EIGHT_HOURS } from "./constants";
 
 export default async function getFundingRateTimeLeft(ticker: string): Promise<number> {
-    const { timeOfLastFunding } = await fundingRatesModel.findOne({ ticker: ticker })
+    const fundingRates = await fundingRatesModel.findOne({ ticker: ticker })
 
-    if (!timeOfLastFunding) {
+    if (!fundingRates) {
         return EIGHT_HOURS
     }
 
-    const timeLeftTo8Hours = (timeOfLastFunding + EIGHT_HOURS) - new Date().getTime()
+    const timeLeftTo8Hours = (fundingRates.timeOfLastFunding + EIGHT_HOURS) - new Date().getTime()
     return timeLeftTo8Hours
 }
