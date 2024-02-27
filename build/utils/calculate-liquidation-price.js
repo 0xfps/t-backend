3a7a2c455841253a7258b0b3404edffb9015c2ff
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-02-27 20:33:19 +0100

    '✨✨✨✨'

--- build/utils/calculate-liquidation-price.js
+++ build/utils/calculate-liquidation-price.js
@@ -3,21 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const constants_1 = require("./constants");
 /**
  * Calculates the liquidation price for long and short
- * positions using the old formulas:
+ * positions using the formulas:
  *
  * Long:
  * Entry price / (1 + (Initial margin ratio / Leverage))
  *
  * Short:
  * Entry price / (1 - (Initial margin ratio / Leverage))
- *
- * These formulas have been updated to:
- *
- * Long:
- * entryPrice - (LIQUIDATION_THRESHOLD / leverage) * -1 * entryPrice
- *
- * Short:
- * entryPrice - (LIQUIDATION_THRESHOLD / leverage) * 1 * entryPrice
  *
  * @param positionType  Position type.
  * @param entryPrice    Entry price of matching order.
@@ -26,12 +18,8 @@ const constants_1 = require("./constants");
  *                      0.01 not 1%.
  */
 function calculateLiquidationPrice(positionType, entryPrice, leverage, marginRatio) {
-    // Old formula.
-    // return positionType == LONG ?
-    //     parseFloat((entryPrice / (1 + (marginRatio / leverage))).toFixed(4))
-    //     : parseFloat((entryPrice / (1 - (marginRatio / leverage))).toFixed(4))
     return positionType == constants_1.LONG ?
-        parseFloat((entryPrice - ((constants_1.LIQUIDATION_THRESHOLD / leverage) * -1 * entryPrice)).toFixed(4))
-        : parseFloat((entryPrice - ((constants_1.LIQUIDATION_THRESHOLD / leverage) * 1 * entryPrice)).toFixed(4));
+        parseFloat((entryPrice / (1 + (marginRatio / leverage))).toFixed(4))
+        : parseFloat((entryPrice / (1 - (marginRatio / leverage))).toFixed(4));
 }
 exports.default = calculateLiquidationPrice;
--- src/utils/calculate-liquidation-price.ts
+++ src/utils/calculate-liquidation-price.ts
@@ -1,22 +1,14 @@
-import { LIQUIDATION_THRESHOLD, LONG, PositionTypes } from "./constants";
+import { LONG, PositionTypes } from "./constants";
 
 /**
  * Calculates the liquidation price for long and short
- * positions using the old formulas:
+ * positions using the formulas:
  * 
  * Long:
  * Entry price / (1 + (Initial margin ratio / Leverage))
  * 
  * Short:
  * Entry price / (1 - (Initial margin ratio / Leverage))
- * 
- * These formulas have been updated to:
- * 
- * Long:
- * entryPrice - (LIQUIDATION_THRESHOLD / leverage) * -1 * entryPrice
- * 
- * Short:
- * entryPrice - (LIQUIDATION_THRESHOLD / leverage) * 1 * entryPrice
  * 
  * @param positionType  Position type.
  * @param entryPrice    Entry price of matching order.
@@ -30,13 +22,7 @@ export default function calculateLiquidationPrice(
     leverage: number,
     marginRatio: number
 ): number {
-
-    // Old formula.
-    // return positionType == LONG ?
-    //     parseFloat((entryPrice / (1 + (marginRatio / leverage))).toFixed(4))
-    //     : parseFloat((entryPrice / (1 - (marginRatio / leverage))).toFixed(4))
-
     return positionType == LONG ?
-        parseFloat((entryPrice - ((LIQUIDATION_THRESHOLD / leverage) * -1 * entryPrice)).toFixed(4))
-        : parseFloat((entryPrice - ((LIQUIDATION_THRESHOLD / leverage) * 1 * entryPrice)).toFixed(4))
+        parseFloat((entryPrice / (1 + (marginRatio / leverage))).toFixed(4))
+        : parseFloat((entryPrice / (1 - (marginRatio / leverage))).toFixed(4))
 }
